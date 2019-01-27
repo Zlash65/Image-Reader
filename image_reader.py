7ce47a5c21ee5d415d32d855d07ac7e437dbae4b
Zlash65 <zarrar65@gmail.com>
2019-01-27 13:36:51 +0530

    blend same image to correct whiteness

--- image_reader.py
+++ image_reader.py
@@ -3,6 +3,14 @@ import pytesseract
 import os, re, sys, cv2
 from PIL import Image
 
+def overlay_blend(image_path):
+	from PIL import Image
+	correctionVal = 0.05 # fraction of white to add to the main image
+	img_file = Image.open(image_path)
+	img_blended = Image.blend(img_file, img_file, correctionVal)
+
+	img_blended.save("temp1.png")
+
 def read_text(image_path, image_obj=None):
 	img = Image.open(image_path) if not image_obj else image_obj
 	imagetext = pytesseract.image_to_string(img)
@@ -18,8 +26,17 @@ if __name__ == '__main__':
 	ap = argparse.ArgumentParser()
 	ap.add_argument("-i", "--image", required=True,
 		help="path to input image to be OCR'd")
+	ap.add_argument("-p", "--preprocess", type=str, default="thresh",
+		help="type of preprocessing to be done")
 	args = vars(ap.parse_args())
 
-	read_text(args["image"])
+	if args["preprocess"]=="blend":
+		overlay_blend(args["image"])
+		read_text("temp1.png")
+	else:
+		read_text(args["image"])
+
+	# remove temporary files
+	os.remove("temp1.png")
 
 	print('\n------ Done -------')
